c7ba83c491b9d2efa85c5282af2b37b3aef2491d
Ruslan <admin@ruslan.beer>
2024-05-07 20:49:55 +0200

    feat(mason): add tool to ensure we have installed packages

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      -- a list of all tools you want to ensure are installed upon
+      -- start
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "json-lsp",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "hadolint",
+        "markdownlint",
+        "marksman",
+        "shfmt",
+        "eslint-lsp",
+
+        -- Python
+        "pyright",
+        "mypy",
+        "ruff",
+        "ruff-lsp",
+        "black",
+        "debugpy",
+        -- "vulture",
+        "isort",
+      },
+      auto_update = true,
+      run_on_start = true,
+    },
+  },
+}
